af7646ad33172cd13715cec590acf78466780654
Maria Mazur <mmazur18@yandex.ru>
2024-01-29 21:45:47 +0300

    fix после ревью 1

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,32 +1,18 @@
-function fnOpenModal(elemnt) {
+function openPopup(elemnt) {
     elemnt.classList.add("popup_is-opened")
-    document.addEventListener('keydown', closeByEscape); 
+    document.addEventListener('keydown', closeByEscape);
 }
 
-function fnCloseModal(elemnt) {
+function closePopup(elemnt) {
     elemnt.classList.remove("popup_is-opened")
-    document.removeEventListener('keydown', closeByEscape); 
+    document.removeEventListener('keydown', closeByEscape);
 }
 
 function closeByEscape(evt) {
     if (evt.key === 'Escape') {
         const openedPopup = document.querySelector('.popup_is-opened')
-        fnCloseModal(openedPopup)
+        closePopup(openedPopup)
     }
 }
 
-function addCardModal(event) {
-    const card = event.target.closest('.card__image');
-    const popupImageImg = document.querySelector(".popup__image");
-    const popupCaption = document.querySelector(".popup__caption");
-    const popupImage = document.querySelector(".popup_type_image");
-
-    popupImageImg.src = card.src;
-    popupImageImg.alt = card.alt;
-    popupCaption.textContent = card.alt;
-
-    popupImage.classList.add("popup_is-opened");
-    document.addEventListener('keydown', closeByEscape); 
-}
-
-export { fnOpenModal, fnCloseModal, addCardModal, closeByEscape}
+export { openPopup, closePopup }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './styles/index.css';
 import { initialCards } from './cards.js';
-import { fnOpenModal, fnCloseModal, addCardModal } from './components/modal.js';
+import { openPopup, closePopup } from './components/modal.js';
 import { deleteCard, createElement, fnLikeButton } from './components/card.js';
 
 const placesList = document.querySelector('.places__list');
-
 const editButton = document.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
 const popupEdit = document.querySelector(".popup_type_edit");
@@ -13,23 +12,27 @@ const popupsClose = document.querySelectorAll(".popup__close");
 const popupImage = document.querySelector(".popup_type_image");
 const popupName = document.querySelector('.popup__input_type_name');
 const popupDescription = document.querySelector('.popup__input_type_description');
-const name = document.querySelector('.popup__input_type_card-name');
-const link = document.querySelector('.popup__input_type_url');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const popupCardName = document.querySelector('.popup__input_type_card-name');
+const popupUrl = document.querySelector('.popup__input_type_url');
+const popupImageImg = document.querySelector(".popup__image");
+const popupCaption = document.querySelector(".popup__caption");
 
 editButton.addEventListener("click", () => {
-    fnOpenModal(popupEdit);
+    openPopup(popupEdit);
     createInfo();
 });
 
 addButton.addEventListener("click", () => {
-    fnOpenModal(popupNewCard);
+    openPopup(popupNewCard);
 });
 
 popupsClose.forEach(popup =>
     popup.addEventListener("click", () => {
-        fnCloseModal(popupEdit);
-        fnCloseModal(popupNewCard);
-        fnCloseModal(popupImage);
+        closePopup(popupEdit);
+        closePopup(popupNewCard);
+        closePopup(popupImage);
     })
 )
 
@@ -37,15 +40,21 @@ document.addEventListener("click", (event) => {
     if (event.target == popupEdit ||
         event.target == popupNewCard ||
         event.target == popupImage) {
-        fnCloseModal(popupEdit)
-        fnCloseModal(popupNewCard)
-        fnCloseModal(popupImage)
+        closePopup(popupEdit)
+        closePopup(popupNewCard)
+        closePopup(popupImage)
     }
 });
 
+function addCardModal(event) {
+    const card = event.target.closest('.card__image');
 
+    popupImageImg.src = card.src;
+    popupImageImg.alt = card.alt;
+    popupCaption.textContent = card.alt;
 
-
+    openPopup(popupImage);
+}
 
 initialCards.forEach(cardInfo => {
     placesList.append(createElement(cardInfo.name, cardInfo.link, deleteCard, fnLikeButton, addCardModal));
@@ -55,30 +64,11 @@ function createCard() {
     const popupNewCard = document.querySelector(".popup_type_new-card");
     const placesList = document.querySelector('.places__list');
 
-    const newCard = {
-        name: name.value,
-        link: link.value
-    }
-
-    initialCards.unshift(newCard)
-
-    fnCloseModal(popupNewCard);
-
-    document.querySelectorAll(".card").forEach(card => card.remove())
-    initialCards.forEach(cardInfo => {
-        placesList.append(createElement(cardInfo.name, cardInfo.link, deleteCard, fnLikeButton, addCardModal));
-    })
+    placesList.prepend(createElement(popupCardName.value, popupUrl.value, deleteCard, fnLikeButton, addCardModal))
+    closePopup(popupNewCard);
 }
 
-
-
-
-
-
 function createInfo() {
-    const profileTitle = document.querySelector('.profile__title');
-    const profileDescription = document.querySelector('.profile__description');
-
     popupName.value = profileTitle.innerText;
     popupDescription.value = profileDescription.innerText;
 }
@@ -89,17 +79,13 @@ function handleFormSubmit(evt) {
     const nameValue = popupName.value;
     const jobValue = popupDescription.value;
 
-    const profileDescription = document.querySelector('.profile__description');
-    const profileTitle = document.querySelector('.profile__title');
-
     profileTitle.textContent = nameValue;
     profileDescription.textContent = jobValue;
 
-    fnCloseModal(popupEdit);
+    closePopup(popupEdit);
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
 
 const formCreateCard = document.forms['new-place']
 formCreateCard.addEventListener('submit', createCard);
-
